843df6860d043552d6d29eba918846a60f604165
Nazar <nazar.shevchuk.121@gmail.com>
2025-06-23 13:22:59 +0300

    sol1

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let ammount = 0;
+
+  return function adder(a) {
+    if (a === undefined) {
+      const sum = ammount;
+
+      ammount = 0;
+
+      return sum;
+    }
+
+    ammount += a;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
